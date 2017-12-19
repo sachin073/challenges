5810a2db0c749f8437b646172a4c99e38266f5d8
sachin073 <verma.sachin@outlook.in>
2017-12-20 01:39:13 +0530

    dec-starts

---
+++ src/Dec_circuit/TwoArrays.java
@@ -0,0 +1,109 @@
+package Dec_circuit;
+
+/*
+1 2 3
+1 2 3
+
+11 22 33
+
+12 12  12 23  23 12  23 23
+
+123 123
+
+
+5 5 1 2 3   if a[i]!=b[j]
+6 5 1 2 3
+
+                        k=1
+55 11 22 33 55
+
+55 65   51 65   51 51   12 51   12 12   23 12   23 23 k1
+
+551 651  551 123  551 512
+
+
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+/**
+ * Created by sachin on 20/12/17.
+ */
+public class TwoArrays {
+
+    static int[] arrayA = null;
+    static int[] arrayB = null;
+
+    public static void main(String[] args) {
+
+        BufferedReader inpReader  = new BufferedReader(new InputStreamReader(System.in));
+
+        int sizeOfA,sizeOfB,k;
+        String temp= null;
+        try {
+            temp = inpReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sizeOfA = Integer.parseInt(temp.split(" ")[0]);
+        sizeOfB = Integer.parseInt(temp.split(" ")[1]);
+        k = Integer.parseInt(temp.split(" ")[2]);
+        arrayA = new int[sizeOfA];
+        arrayB = new int[sizeOfB];
+
+        try {
+
+            temp = inpReader.readLine();
+            int i=0;
+            for (String string :temp.split(" ")) {
+                arrayA[i]=Integer.parseInt(string.trim());
+            }
+
+            temp = inpReader.readLine();
+            i=0;
+            for (String string :temp.split(" ")) {
+                arrayB[i]=Integer.parseInt(string.trim());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        HashSet<String> sampleSpace = new HashSet<>();
+
+        int[] tempSpace = new int[sizeOfA];
+        for (int i = 0; i < sizeOfA; i++) { //
+             //fill array from 0 to iteration
+            for (int j = 0; j ==i ; j++) {
+                tempSpace[j] = arrayA[j];
+            }
+
+            for (int x = 0; x < sizeOfB; x++) {
+
+            }
+        }
+    }
+
+    private static int findMatchings(int[] matcher ){
+     int [] copyOfB = arrayB.clone();
+        int sizeofB= copyOfB.length;
+        int sizeOfMatcher= matcher.length;
+        //find one pattern and remove one from start or remove from till start of found -1
+        for (int i = 0; i < sizeofB ; i++) {
+            for (int j = 0; j < sizeOfMatcher ; j++) {
+                    if (copyOfB[i] == matcher[j]){
+
+                    }
+            }
+        }
+
+        return 0;
+    }
+
+
+}
+
+

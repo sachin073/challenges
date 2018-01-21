32c5e51a792ed496b59c19e038cd992782a17d34
sachin073 <verma.sachin@outlook.in>
2018-01-22 00:31:58 +0530

    jan circuit

--- src/jan_circuit/Roads.java
+++ src/jan_circuit/Roads.java
@@ -1,7 +1,103 @@
 package jan_circuit;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by sachin on 21/1/18.
+
+
+ 1 2 50 6 2 1 2 3 4 4 1
+
+ 1 2  3 4 5 6 7 8
+ 1 50 6 2 6 2 3     K =5
+
+ 1 3
+ 1 4 5
+ 1 4 6 7
+
+
+
+
+ 1
+ 1 2 4
+ 1 2 4            5
+ 1 2 5 6
+ 1 2 5 7 8
+
+
+ 1 2 5 6 7 8    k 4  elements 6 max 8
+
+ break if max elements >  max elements now found , work max elements <= max now
+
+ 1 2 6 7 8
+
+
+
+
+ 1 > 2 3 4 5 6 7 8
+
+
+ 2 > 3 4 5 6 7 8
+ 3 > 4 5 6 7 8
+ 4 > 5 6 7 8
+ 5 > 6 7 8
+ 6 > 7 8
+ 7 > 8
+
+
+
+
  */
 public class Roads {
+
+    static String inp="";
+    static BufferedReader inpReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) {
+
+        int len, time;
+
+        try {
+            inp = inpReader.readLine();
+            len = Integer.parseInt(inp.split(" ")[0]);
+            time = Integer.parseInt(inp.split(" ")[1]);
+
+            int[] arr = new int[len];
+            inp = inpReader.readLine();
+            String[] strings = inp.split(" ");
+
+            int source =Integer.parseInt(strings[0]);
+            for (int i = 1; i < strings.length; i++) {
+                arr[i] = Integer.parseInt(strings[i]);
+            }
+
+            Arrays.sort(arr);
+
+            int points = 1;
+
+            for (int i = 1; i < arr.length; i++) {
+
+                if (time ==0){
+                    break;
+                }
+                if ( Math.abs(arr[i] - source)  <= time){
+                    source = arr[i];
+                    time = time - Math.abs(arr[i] -source);
+                    points++;
+                }
+
+            }
+
+            System.out.println(points);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/jan_circuit/congruenceRelation.java
+++ src/jan_circuit/congruenceRelation.java
@@ -1,7 +1,59 @@
 package jan_circuit;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by sachin on 20/1/18.
  */
 public class congruenceRelation {
+
+    static     String inp="";
+    static BufferedReader inpReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) {
+
+        int number=0;
+        int div =0;
+
+        try {
+
+            inp = inpReader.readLine();
+            number= Integer.parseInt(inp.split(" ")[0]);
+            div= Integer.parseInt(inp.split(" ")[1]);
+
+            long [] divs =new long[div];
+
+            System.out.println(System.nanoTime()/1000000000);
+            for (int i = 1 ; i <= number; i++) {
+                 int x = i & (div -1 );
+                //int x = i%div;
+                divs[x] =++divs[x];
+            }
+            System.out.println(System.nanoTime()/1000000000);
+
+            long outCount=0;
+            for (long elementCount : divs) {
+                long sum =0;
+                for (int i = 1; i < elementCount; i++) {
+                    sum +=i;
+                }
+                outCount = outCount + sum;
+
+            }
+
+            System.out.println(System.nanoTime()/1000000000);
+
+            System.out.println(outCount);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
 }
